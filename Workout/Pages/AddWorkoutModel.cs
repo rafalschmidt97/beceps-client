a17c69a78178112faa51d2460fcd2d10fa2d48d3
Alex Toryanik <alex1toryanik@gmail.com>
2020-02-11 21:36:20 +0200

    feature: add AddWorkout view and model

---
+++ Workout/Pages/AddWorkoutModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Beceps.Workout.Pages
+{
+    public class AddWorkoutModel
+    {
+        [Required]
+        [StringLength(25, ErrorMessage = "Name is too long.")]
+        public string Name { get; set; }
+    }
+}
