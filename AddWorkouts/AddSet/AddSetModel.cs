e1fba44e4a01adad7f08137b3f7b5fec217f43a6
Alex Toryanik <alex1toryanik@gmail.com>
2020-02-12 18:32:33 +0200

    feature: add AddSet view and model

---
+++ AddWorkouts/AddSet/AddSetModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Beceps.AddWorkouts.AddSet
+{
+    public class AddSetModel
+    {
+        [Required]
+        [StringLength(25, ErrorMessage = "Name is too long.")]
+        public string Name { get; set; }
+        [Required]
+        [Range(0, 10)]
+        public int? NumberOfSets { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public int? NumberOfReps { get; set; }
+    }
+}
