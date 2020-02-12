e462ddad2d9768afef06f009ac99544f4b6719ad
Alex Toryanik <alex1toryanik@gmail.com>
2020-02-12 18:18:57 +0200

    feature: add AddRoutine view and model
    fix: change page directive in AddRoutine

---
+++ AddWorkouts/AddRoutine/AddRoutineModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Beceps.AddWorkouts.AddRoutine
+{
+    public class AddRoutineModel
+    {
+        [Required]
+        [StringLength(25, ErrorMessage = "Name is too long.")]
+        public string Name { get; set; }
+
+        [Required]
+        public DayOfWeek WeekDay { get; set; }
+    }
+}
