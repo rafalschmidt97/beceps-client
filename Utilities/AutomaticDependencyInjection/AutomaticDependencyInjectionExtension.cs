f27e1f8ee18138f525e8deb407627e90cf08b7e2
Alex Toryanik <alex1toryanik@gmail.com>
2020-02-13 16:52:58 +0200

    features: add automatic dependency injection
    feature: add scruptor

---
+++ Utilities/AutomaticDependencyInjection/AutomaticDependencyInjectionExtension.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Beceps.Utilities.AutomaticDependencyInjection
+{
+    public static class AutomaticDependencyInjectionExtension
+    {
+        public static void AddDependencies(this IServiceCollection services)
+        {
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            services.Scan(scan => scan
+                .FromAssemblies(assemblies)
+                .AddClasses(classes => classes.AssignableTo<ITransient>())
+                .AsImplementedInterfaces()
+                .WithTransientLifetime()
+                .AddClasses(classes => classes.AssignableTo<IScoped>())
+                .AsImplementedInterfaces()
+                .WithScopedLifetime()
+                .AddClasses(classes => classes.AssignableTo<ISingleton>())
+                .AsImplementedInterfaces()
+                .WithSingletonLifetime());
+        }
+    }
+}
