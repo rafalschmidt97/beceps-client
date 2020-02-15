2a5d537b703681244d8f4c74a16f034d923b57e0
Alex Toryanik <alex1toryanik@gmail.com>
2020-02-15 13:05:04 +0200

    feature: add httpClientExtensions

---
+++ Utilities/HttpClientExtensions.cs
@@ -0,0 +1,93 @@
+﻿using System.Net.Http.Headers;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Text;
+
+namespace Beceps.Utilities
+{
+    public static class HttpClientExtensions
+    {
+        public static async Task<T> GetAsync<T>(this HttpClient httpClient, string url, bool setAuthenticationHeader = true)
+        {
+            httpClient.DefaultRequestHeaders
+              .Accept
+              .Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            var request = GetDefaultHttpRequestMessage(url, setAuthenticationHeader);
+
+            request.Method = new HttpMethod("GET");
+
+            var response = await httpClient.SendAsync(request);
+            var responseBytes = await response.Content.ReadAsByteArrayAsync();
+            return JsonSerializer.Deserialize<T>(responseBytes, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        }
+
+        public static async Task<T> Post<T>(this HttpClient httpClient, string url, object content, bool setAuthenticationHeader = true)
+        {
+            httpClient.DefaultRequestHeaders
+              .Accept
+              .Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            var request = GetDefaultHttpRequestMessage(url, setAuthenticationHeader);
+
+            request.Method = new HttpMethod("POST");
+
+            request.Content = SerializeContent(content);
+
+            var response = await httpClient.SendAsync(request);
+            var responseBytes = await response.Content.ReadAsByteArrayAsync();
+            return JsonSerializer.Deserialize<T>(responseBytes, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        }
+
+        public static async Task<T> PutAsync<T>(this HttpClient httpClient, string url, object content, bool setAuthenticationHeader = true)
+        {
+            httpClient.DefaultRequestHeaders
+              .Accept
+              .Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            var request = GetDefaultHttpRequestMessage(url, setAuthenticationHeader);
+
+            request.Method = new HttpMethod("PUT");
+            request.Content = SerializeContent(content);
+
+            var response = await httpClient.SendAsync(request);
+            var responseBytes = await response.Content.ReadAsByteArrayAsync();
+            return JsonSerializer.Deserialize<T>(responseBytes, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        }
+
+        public static async Task<T> DeleteAsync<T>(this HttpClient httpClient, string url, object content, bool setAuthenticationHeader = true)
+        {
+            httpClient.DefaultRequestHeaders
+              .Accept
+              .Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            var request = GetDefaultHttpRequestMessage(url, setAuthenticationHeader);
+
+            request.Method = new HttpMethod("DELETE");
+            request.Content = SerializeContent(content);
+
+            var response = await httpClient.SendAsync(request);
+            var responseBytes = await response.Content.ReadAsByteArrayAsync();
+            return JsonSerializer.Deserialize<T>(responseBytes, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        }
+
+        private static HttpRequestMessage GetDefaultHttpRequestMessage(string url, bool setAuthenticationHeader = true)
+        {
+            var request = new HttpRequestMessage()
+            {
+                RequestUri = new System.Uri($"http://localhost:8080/{url}"),
+            };
+
+            if (setAuthenticationHeader)
+            {
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwibmJmIjoxNTgxNjA0MTIyLCJyb2xlcyI6W10sImlzcyI6ImJlY2VwcyIsImV4cCI6MTcwNTA2MDkxMSwiaWF0IjoxNTgxNjA0MTIyfQ.0yeeiEcn8Z1KMm48sf0FZUvKIibatQFgyA1SFWk9xlk");
+            }
+
+            return request;
+        }
+
+        private static HttpContent SerializeContent(object content)
+        {
+            return new StringContent(JsonSerializer.Serialize(content, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase }),
+                                    Encoding.UTF8,
+                                    "application/json");
+        }
+    }
+}
