b5d45c182b257f3bd8ef451eeb3b9756474ae2c9
Alex Toryanik <alex1toryanik@gmail.com>
2020-05-06 13:03:29 +0300

    feature: sign-in

---
+++ Authentication/SignIn/SignInModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Beceps.Authentication.SignIn
+{
+    public class SignInModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public void Reset()
+        {
+            Email = "";
+            Password = "";
+        }
+    }
+}
