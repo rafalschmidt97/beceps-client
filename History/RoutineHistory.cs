0260833472e0f672c7ca3d60170b86c63910dd4a
Alex Toryanik <alex1toryanik@gmail.com>
2020-05-06 13:04:05 +0300

    feature: history

---
+++ History/RoutineHistory.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Beceps.History
+{
+    public class RoutineHistory
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<ExerciseHistory> Exercises { get; set; }
+    }
+}
---
+++ History/WorkoutHistory.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Beceps.History
+{
+    public class WorkoutHistory
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IList<RoutineHistory> Routines { get; set; }
+    }
+}
