d3e0a6809941793d4688e16f0a27f23c81984edb
Alex Toryanik <alex1toryanik@gmail.com>
2020-05-06 13:03:51 +0300

    feature: sign-up

---
+++ Authentication/SignUp/SignUpModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Beceps.Authentication.SignUp
+{
+    public class SignUpModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public void Reset()
+        {
+            Email = "";
+            Password = "";
+        }
+    }
+}
